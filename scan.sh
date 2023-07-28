d043101907b9223da15dad0a6ec87c7adafa0e49
Alex Nichol <unixpickle@gmail.com>
2023-07-28 16:45:26 -0400

    scan over n_latent

--- scan.sh
+++ scan.sh
@@ -1,15 +1,21 @@
 mkdir -p logs
-python3 -m rebar.scripts.train_mnist \
-    --epochs 100 \
-    --log logs/rebar.npz
-python3 -m rebar.scripts.train_mnist \
-    --epochs 100 \
-    --init_eta 0.000001 \
-    --cv_lr 0 \
-    --log logs/reinforce.npz
-python3 -m rebar.scripts.train_mnist \
-    --epochs 100 \
-    --init_eta 0.000001 \
-    --cv_lr 0 \
-    --sample_baseline \
-    --log logs/baseline.npz
+epochs=150
+for n_latent in 32 64 128 256; do
+    python3 -m rebar.scripts.train_mnist \
+        --epochs $epochs \
+        --n_latent $n_latent \
+        --log logs/rebar_${n_latent}.npz
+    # python3 -m rebar.scripts.train_mnist \
+    #     --epochs 100 \
+    #     --n_latent $n_latent \
+    #     --init_eta 0.000001 \
+    #     --cv_lr 0 \
+    #     --log logs/reinforce_${n_latent}.npz
+    python3 -m rebar.scripts.train_mnist \
+        --epochs $epochs \
+        --n_latent $n_latent \
+        --init_eta 0.000001 \
+        --cv_lr 0 \
+        --sample_baseline \
+        --log logs/baseline_${n_latent}.npz
+done
